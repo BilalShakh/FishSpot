3a97c597c7a039784d35b6ac8352ecc3adca310d
Yichun Liu <Yichun>
2021-10-04 21:26:45 -0400

    initial commit for search and result

---
+++ src/components/Result.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Result() {
+  return (
+    <div className="result">
+      <div class="container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Result;
---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Search() {
+  return (
+    <div className="search">
+      <div class="container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Search;
