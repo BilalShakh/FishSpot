53ed5eb7fc3c5b9ec6715da26221bb66e9e9445d
Yichun Liu <Yichun>
2021-10-05 22:58:42 -0400

    setting up result page

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,10 +4,60 @@ import React from "react";
 function Result() {
   return (
     <div className="result">
-      <div class="container">
-        
+      <div class=".container-fluid">
+        <div class ="row">
+          <div class="col-sm-2">
+            
+          </div>
+          <div class="col-sm-8">
+            <img src="../assets/images/resultsamplemap.jpg" alt="google map result" style="width:100%"/>
+          </div>
+          <div class="col-sm-2">
+            
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-2">
+            
+          </div>
+          <div class="col-sm-2">
+            .col-sm-4
+          </div>
+          <div class="col-sm-2">
+            .col-sm-4
+          </div>
+          <div class="col-sm-2">
+            .col-sm-4
+          </div>
+          <div class="col-sm-2">
+            .col-sm-4
+          </div>
+          <div class="col-sm-2">
+            
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-2">
+            
+          </div>
+          <div class="col-sm-2">
+            .col-sm-4
+          </div>
+          <div class="col-sm-2">
+            .col-sm-4
+          </div>
+          <div class="col-sm-2">
+            .col-sm-4
+          </div>
+          <div class="col-sm-2">
+            .col-sm-4
+          </div>
+          <div class="col-sm-2">
+            
+          </div>
+        </div>
       </div>
-    </div>
+  </div>
   );
 }
 
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -26,6 +26,7 @@ function Search() {
         </form>
       </div>
     </div>
+    
   );
 }
 
