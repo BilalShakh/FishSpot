eb7f0bef57275c4056be6e3799f3858b746cde6f
Yichun Liu <Yichun>
2021-10-06 23:12:21 -0400

    adding more content for result page

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -7,57 +7,59 @@ function Result() {
       <div class=".container-fluid">
         <div class ="row">
           <div class="col-sm-2">
-            
           </div>
           <div class="col-sm-8">
             <img src="../assets/images/resultsamplemap.jpg" alt="google map result" style="width:100%"/>
           </div>
           <div class="col-sm-2">
-            
           </div>
         </div>
         <div class="row">
           <div class="col-sm-2">
-            
           </div>
           <div class="col-sm-2">
-            .col-sm-4
+            <img src="../assets/images/samplespotimage1-1.jpg" alt="google map result"/>
+            <span>Jacky Fishingspot</span>
           </div>
           <div class="col-sm-2">
-            .col-sm-4
+            <img src="../assets/images/samplespotimage1-2.jpg" alt="google map result"/>
+            <span> Lake Niagara</span>
           </div>
           <div class="col-sm-2">
-            .col-sm-4
+            <img src="../assets/images/samplespotimage2-1.jpg" alt="google map result"/>
+            <span>Lake Carlisle</span>
           </div>
           <div class="col-sm-2">
-            .col-sm-4
+            <img src="../assets/images/spot2-2.jpg" alt="google map result"/>
+            <span>Paris Pond</span>
           </div>
           <div class="col-sm-2">
-            
           </div>
         </div>
         <div class="row">
           <div class="col-sm-2">
-            
           </div>
           <div class="col-sm-2">
-            .col-sm-4
+            <img src="../assets/images/spot2-3.jpg" alt="google map result"/>
+            <span>Jacky Fishing Spots</span>
           </div>
           <div class="col-sm-2">
-            .col-sm-4
+            <img src="../assets/images/spot3-1.jpg" alt="google map result"/>
+            <span>Niapenco Fishing Spots</span>
           </div>
           <div class="col-sm-2">
-            .col-sm-4
+            <img src="../assets/images/spot3-2.jpg" alt="google map result"/>  
+            <span>Jacky Fishingspot</span>  
           </div>
           <div class="col-sm-2">
-            .col-sm-4
+            <img src="../assets/images/spot4-1.jpg" alt="google map result"/>
+            <span>Cooper Creek</span>
           </div>
           <div class="col-sm-2">
-            
           </div>
         </div>
       </div>
-  </div>
+    </div>
   );
 }
 
