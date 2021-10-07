2f30589e41bc6f14a206c6c8783d6a2670a20311
BilalShakh <bilalzaki92@gmail.com>
2021-10-07 01:04:54 -0400

    made some progress on Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import fishing_1 from '../../Assets/fishing_1.png'
+import { VStack, Box, Image, HStack } from "@chakra-ui/react"
 import "./Home.css"
 
 export default class Home extends Component {
@@ -11,6 +13,14 @@ export default class Home extends Component {
         return (
             <div class = "home-background">                
                 <Header></Header>
+                <VStack>
+                    <HStack>
+                        <Box maxW="200px" borderWidth="1px" borderRadius="lg" overflow="hidden">
+                            <Image src={fishing_1} alt="IMAGE NOT FOUND"/>
+
+                        </Box>
+                    </HStack>
+                </VStack>
                 <Footer></Footer>
             </div>
         )
