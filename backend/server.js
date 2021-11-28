4bc6ee4f8bcfc891aa6426ead16f957e46e1bc44
BilalShakh <bilalzaki92@gmail.com>
2021-11-28 19:53:52 +0000

    Successfully connected to the DB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mysql = require('mysql2')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express()
 
@@ -11,6 +13,12 @@ const pool = mysql.createPool({
 })
 
 pool.getConnection( (err, connection)=> {
-  if (err) throw (err)
+  if (err){
+    console.log(err)
+  }else {
   console.log ("DB connected successful: " + connection.threadId)
+  }
 })
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port ${port}`))
