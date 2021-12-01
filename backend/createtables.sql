3cc784a45cedc4342fb514e183d10cded0053d95
Yichun Liu <Yichun>
2021-11-30 22:47:17 -0500

    defined tables

--- backend/createtables.sql
+++ backend/createtables.sql
@@ -6,3 +6,20 @@ CREATE TABLE UserAccounts (
     Password varchar(255) not null,
     primary key (UserID)
 );
+
+CREATE TABLE FISHSPOTS (
+    ObjectID int not null,
+    Name varchar(255) not null,
+    Description varchar(5000) not null,
+    Latitude float(20) not null,
+    Longitude float(20) not null
+    PRIMARY KEY (ObjectID)
+);
+
+CREATE TABLE REVIEWS (
+    ReviewID int not null,
+    Content varchar(5000) not null,
+    UserId int not null,
+    PRIMARY KEY (ReviewID)
+    FOREIGN KEY (UserID)
+);
