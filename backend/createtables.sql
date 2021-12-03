53ae80bbf497a070ead3c51ff7c485ebaa34b0e7
Yichun Liu <Yichun>
2021-12-02 21:49:32 -0500

    finished initial set up of table and records

--- backend/createtables.sql
+++ backend/createtables.sql
@@ -20,8 +20,9 @@ CREATE TABLE FISHSPOTS (
 CREATE TABLE REVIEWS (
     ReviewID int not null auto_increment,
     Content varchar(5000) not null,
-    UserId int not null,
+    UserID int not null,
     ObjectID int not null,
+    Rating int not null,
     PRIMARY KEY (ReviewID),
     FOREIGN KEY (UserID) REFERENCES UserAccounts(UserID),
     FOREIGN KEY (ObjectID) REFERENCES FISHSPOTS(ObjectID)
@@ -35,14 +36,24 @@ VALUES
 ('Mathrew Alex','455444ww3@gmail.com','fbhs','FVJT67ECFjj'),
 ('Peter Apple','fhvhecvg@gmail.com','wefhf','rcd8JNM7887');
 
-INSERT INTO FISHSPOTS(Name,Decription,Latitude,Longitude,Image_File_Name)
+INSERT INTO FISHSPOTS(Name,Description,Latitude,Longitude,Image_File_Name)
 VALUES
 ('Credit River','This is flat and slow water like fishing in a lake. The fishing can be good in the spring for steelhead and in September for huge salmon. Anglers casting lures can do very well here if they hit the runs of salmon just as they start. A favorite lure for this area is spoons like the Little Cleos which can be cast far out into the river and lake.',43.5557682874319,-79.60028572660218,'fishing_6.jpg'),
-('Montana Mountain','',33.40541925484954, -111.15446030854552,'fishing_1'),
-('','',,''),
-('','',,''),
-('','',,''),
-('','',,''),
-('','',,''),
-('','',,''),
+('Montana Mountain',' Anglers casting lures can do very well here if they hit the runs of salmon just as they start. A favorite lure for this area is spoons like the Little Cleos which can be cast far out into the river and lake. Large crankbaits and inline spinners can be very good too',33.40541925484954, -111.15446030854552,'fishing_1.jpg'),
+('Kawartha Lakes','The fishing can be good in the spring for steelhead and in September for huge salmon. Anglers casting lures can do very well here if they hit the runs of salmon just as they start. A favorite lure for this area is spoons like the Little Cleos which can be cast far out into the river and lake.',44.5597940516632, -78.91889076442398,'fishing_2.jpg'),
+('Port Hope','The fishing can be good in the spring for steelhead and in September for huge salmon. Anglers casting lures can do very well here if they hit the runs of salmon just as they start. A favorite lure for this area is spoons like the Little Cleos which can be cast far out into the river and lake.',44.014891817625966, -78.38876293375819,'fishing_3.jpg'),
+('Rainy River','This is flat and slow water like fishing in a lake. The fishing can be good in the spring for steelhead and in September for huge salmon. Anglers casting lures can do very well here if they hit the runs of salmon just as they start. A favorite lure for this area is spoons like the Little Cleos which can be cast far out into the river and lake.',48.71584283444749, -94.57394933831512,'fishing_4.jpg'),
+('Jack Fishing Spots','The fishing can be good in the spring for steelhead and in September for huge salmon. Anglers casting lures can do very well here if they hit the runs of salmon just as they start. A favorite lure for this area is spoons like the Little Cleos which can be cast far out into the river and lake.',43.44305458445543, -80.41718736809976,'fishing_5.jpg');
+
+INSERT INTO REVIEWS(Content,UserID,ObjectID,Rating)
+VALUES
+('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat ut facilisis rutrum donec tristique mauris est ac nunc. Eget nec, lorem mi libero accumsan',1,1,3),
+('Salmon run seasonally and in great numbers',2,2,4)
+('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat ut facilisis rutrum donec tristique mauris est ac nunc. Eget nec, lorem mi libero accumsan',2,1,4)
+('Trouts roam free in the rivers. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat ut facilisis rutrum donec tristique mauris est ac nunc.',3,1,5)
+('Salmon run seasonally and in great numbers. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat ut facilisis rutrum donec tristique mauris est ac nunc.',4,1,4)
+('Bass and trout that exist in the deep. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Erat ut facilisis rutrum donec tristique mauris est ac nunc.',5,1,4)
+
+
+
 
