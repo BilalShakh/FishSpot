11c96139e83b68e53f5d83550328d2735aab2e58
BilalShakh <bilalzaki92@gmail.com>
2021-12-05 02:49:33 -0500

    Added code for rating lookup

--- backend/routes/Search.js
+++ backend/routes/Search.js
@@ -13,6 +13,28 @@ async function LookupSpotsByName(name) {
     return Data[0];
 }
 
+async function LookupSpotsByRating(rating) {
+    const params = [parseInt(rating)]
+    let Data = []
+    try {
+      Data = await runQuery("SELECT ObjectID, Name, Feature, Description, LocLatitude, LocLongitude, Image_Key FROM FishingSpots WHERE ObjectID IN (SELECT ObjectID FROM (SELECT ObjectID, AVG(Rating) as avg FROM Reviews GROUP BY ObjectID) as alias2 WHERE FlOOR(alias2.avg) = ?) ", params);
+    } catch(error) {
+      console.log(error);
+    };
+    return Data[0];
+}
+
+async function getAllSpots() {
+    const params = []
+    let Data = []
+    try {
+      Data = await runQuery("SELECT ObjectID, Name, Feature, Description, LocLatitude, LocLongitude, Image_Key  FROM FishingSpots", params);
+    } catch(error) {
+      console.log(error);
+    };
+    return Data[0];
+}
+
 async function getReviewDetails(ObjectID) {
     const params = [ObjectID]
     let Data = []
@@ -48,6 +70,65 @@ function distance(lat1, lat2, lon1, lon2)
 
     return(c * r);
 }
+
+router.get('/rating/:rating',  async (req, res) => {
+    //console.log(req.params)
+    let Data = [];
+    if (rating === "All"){
+        Data = await getAllSpots();
+    } else {
+        Data = await LookupSpotsByRating(rating);
+    }
+    const rating = req.params.rating
+    const locationGiven = req.header("location");
+    const longitude = req.header("lng");
+    const latitude = req.header("lat");
+    let ResData = []
+  
+    if (Data.length !== 0){
+      if (locationGiven){
+          for (let i = 0; i < Data.length; i++){
+                let element = Data[i];
+                const dist = distance(latitude, element.LocLatitude, longitude, element.LocLongitude);
+                if (dist <= 100) {
+                    let reviewDetails = await getReviewDetails(element.ObjectID);
+                    ResData.push({
+                        ObjectID: element.ObjectID,
+                        Name: element.Name, 
+                        Feature: element.Feature, 
+                        Rating: reviewDetails.avgRating, 
+                        NumReviews: reviewDetails.numReviews,
+                        Latitude: element.LocLatitude,
+                        Longitude: element.LocLongitude,
+                        Description: element.Description.substring(0,50),
+                        Image_key: element.Image_Key
+                    });
+                }
+          }
+          res.send({ found: true, cardData: ResData });
+      } else {
+        for (let i = 0; i < Data.length; i++){
+            let element = Data[i];
+            let reviewDetails = await getReviewDetails(element.ObjectID);
+            ResData.push({
+                ObjectID: element.ObjectID,
+                Name: element.Name, 
+                Feature: element.Feature, 
+                Rating: reviewDetails.avgRating, 
+                NumReviews: reviewDetails.numReviews,
+                Latitude: element.LocLatitude,
+                Longitude: element.LocLongitude,
+                Description: element.Description.substring(0,50),
+                Image_key: element.Image_Key
+            });
+        }
+        //Data.forEach(await populateResArray(element));
+        res.send({ found: true, cardData: ResData });
+      }
+    } else {
+      res.send({ found: false });
+    }
+  })
 
 router.get('/name/:term',  async (req, res) => {
     //console.log(req.params)
